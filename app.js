c8f9f3b3487bc1912542303fd843d0a5744c90b9
Eric Zimmer <zimmer.eric@gmail.com>
2018-04-30 11:26:41 -0700

    added authentication with login and register views

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
 
-var   express     = require("express"),
-      app         = express(),
-      bodyParser  = require("body-parser"),
-      mongoose    = require("mongoose"),
-      Campground  = require("./models/campground"),
-      seedDB      = require("./seed"),
-      Comment     = require("./models/comment");
+const express                 = require("express"),
+      app                     = express(),
+      bodyParser              = require("body-parser"),
+      mongoose                = require("mongoose"),
+      passport                = require("passport"),
+      LocalStrategy           = require("passport-local"),
+      
+      Campground              = require("./models/campground"),
+      seedDB                  = require("./seed"),
+      Comment                 = require("./models/comment"),
+      User                    = require("./models/user");
 
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-seedDB();
+//seedDB();
 
 
+//PASSPORT CONFIG
+app.use(require("express-session")({
+   secret: "Sabres are very very very bad but not forever!",
+   resave: false,
+   saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//MIDDLEWARE==================================================
+
+// caching disabled for every route so you cannot go back after logout
+app.use(function(req, res, next) {
+   res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+   next();
+});
+
+//check if logged in before display dashboard
+function isLoggedIn(req, res, next) {
+   if(req.isAuthenticated()) {
+      return next();
+   }
+   res.redirect("/login");
+}
+
+//store user in locals
+app.use( (req, res, next) => {
+   res.locals.currentUser = req.user;
+   next();
+});
+//============================================================
+
+//ROUTES
 app.get("/", (req, res)=> {
    res.render("landing");
 });
@@ -25,7 +66,8 @@ app.get("/campgrounds", (req, res) => {
       if(err){
          console.log(err);
       } else{
-         res.render("campgrounds/index", {campgrounds: allCampgrounds});
+         res.render("campgrounds/index", 
+         {campgrounds: allCampgrounds, currentUser: req.user});
       }
    });
    
@@ -64,7 +106,8 @@ app.get("/campgrounds/:id", (req, res) => {
 });
 
 //COMMENTS ROUTES
-app.get("/campgrounds/:id/comments/new", (req, res) => {
+//must be logged in (isLoggedIn) to comment
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req, res) => {
    Campground.findById(req.params.id, (err, campground) => {
       if(err) {
          console.log(err);
@@ -72,11 +115,10 @@ app.get("/campgrounds/:id/comments/new", (req, res) => {
          res.render("comments/new", {campground: campground});
       }
    });
-   res.render("comments/new");
 });
 
 
-app.post("/campgrounds/:id/comments", (req, res) => {
+app.post("/campgrounds/:id/comments", isLoggedIn, (req, res) => {
    //lookup campground by id
    Campground.findById(req.params.id, (err, campground) => {
       if(err) {
@@ -94,12 +136,50 @@ app.post("/campgrounds/:id/comments", (req, res) => {
          });
       }
    });
-   
-
    //connect new comment to campground
 
 });
 
+//AUTH ROUTES
+
+//show register form
+app.get("/register", (req, res) =>{
+   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+   var newUser = new User({username: req.body.username});
+   User.register(newUser, req.body.password, (err, user) => {
+      if(err) {
+         console.log(err);
+         return res.render("register");
+      } 
+      passport.authenticate("local")(req, res, () => {
+         res.redirect("/campgrounds");
+      });
+   });
+});
+
+//LOGIN ROUTES
+app.get("/login", (req, res) => {
+   res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", 
+   {
+      successRedirect : "/campgrounds",
+      failureRedirect : "/login"
+   }),
+   (req, res) => {
+
+   }
+);
+
+//LOGOUT
+app.get("/logout", (req, res) => {
+   req.logout();
+   res.redirect("/");
+});
 
 app.listen(3000, () =>{
    console.log("working");
